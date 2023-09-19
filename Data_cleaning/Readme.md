5f48c5f2d526fd579b2eb382e70c887ec38a76a6
Imtiaz Siddiqui <osama.siddiqui2017@gmail.com>
2023-09-19 22:08:15 +0500

    Converted Script to Flask app

---
+++ Data_cleaning/Readme.md
@@ -0,0 +1,151 @@
+# Flask Case Management App
+
+This Flask application provides a simple case management system that allows you to retrieve case information and add new cases. It includes two GET endpoints for retrieving case details and one POST endpoint for adding new cases.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Getting Started](#getting-started)
+- [API Endpoints](#endpoints)
+  - [Retrieve Case Text](#retrieve-case-text)
+  - [Retrieve Case Laws](#retrieve-case-laws)
+  - [Add New Case](#add-new-case)
+- [Usage](#usage)
+- [Error Handling](#Error-Handling)
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x installed.
+- Flask installed.
+- SQLAlchemy installed.
+- spaCy installed and the English language model.
+- MySQL or another compatible database server running locally or with the appropriate connection details.
+
+## Getting Started
+
+1. Clone this repository to your local machine.
+```bash
+git clone https://github.com/your-username/flask-case-management-app.git
+cd flask-case-management-app 
+```
+
+
+2. Edit the database connection details in the Flask app (`app.py`) to match your local MySQL database.
+
+3. Run the Flask app.
+```bash
+python main.py
+
+```
+
+The app should now be running at `http://localhost:5000`.
+
+## Endpoints
+
+### Retrieve Case Text and Laws
+
+#### GET `/CaseId/text`
+
+This endpoint allows you to retrieve the clean text of a case by providing the `CaseId` as a parameter.
+
+- **Method**: GET
+- **Parameters**: `CaseId` (integer)
+##### Example Usage
+
+```bash
+{
+  http://localhost:5000/1/text
+}
+```
+
+##### Example Response
+
+```bash
+{
+  "CaseId": 1,
+  "CleanText": "This is the clean text of case 1."
+}
+```
+
+#### GET `/CaseId/laws`
+
+This endpoint allows you to retrieve the clean text of a case by providing the `CaseId` as a parameter.
+
+- **Method**: GET
+- **Parameters**: `CaseId` (integer)
+##### Example Usage
+
+```bash
+{
+  http://localhost:5000/1/laws
+}
+```
+
+##### Example Response
+
+```bash
+{
+  "CaseId": 1,
+  "Laws": "Law A, Law B, Law C"
+}
+
+```
+### Add New Case
+
+POST /addcase
+This endpoint allows you to add a new case to the database by providing a JSON payload with the case details.
+
+- Method: POST
+- Request Payload (JSON):
+-  Id (integer): The unique identifier of the case.
+-  Judgement (string): The text of the case judgment.
+
+
+##### Example Request
+
+```bash
+{
+  curl -X POST -H "Content-Type: application/json" -d '{"Id": 2, "Judgement": "This is a new case judgment."}' http://localhost:5000/addcase
+}
+
+```
+##### Example Responce
+```bash
+{
+  "message": "Case added successfully"
+}
+
+```
+
+### Usage
+
+- Use the /CaseId/text endpoint to retrieve the clean text of a case.
+- Use the /CaseId/laws endpoint to retrieve the laws associated with a case.
+- Use the /addcase endpoint to add new cases to the system.
+
+### Error and Exception Handling
+
+In case of errors, the API will respond with appropriate HTTP status codes and error messages. Refer to the respective endpoint descriptions for details on error responses.
+
+##### For Get requests
+
+```bash
+{
+  200 : Requested data fetched Successfully
+  404 : Requested data not found
+}
+
+```
+
+##### For Post requests
+
+```bash
+{
+  200 : Case data added Successfully
+  400 : Invalid data provided 
+  500 : Exception Occured 
+}
+
+```
--- Data_cleaning/main.py
+++ Data_cleaning/main.py
@@ -1,66 +1,65 @@
+import os
+import re
 import pandas as pd
-
 from lxml import html
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
-import re
-import json
 import spacy
+from flask import Flask, jsonify, request, g
+
+app = Flask(__name__)
+app.secret_key = "!secret_key"
 
 nlp = spacy.load("en_core_web_sm")
 
+# Database connection engine
+engine = create_engine("mysql+pymysql://root:@localhost/taxin_revivedtaxindia_new1", pool_size=10, max_overflow=20)
+
+Session = sessionmaker(bind=engine)
+
+# Create a global database session
+@app.before_request
+def before_request():
+    g.session = Session()
+
+@app.teardown_appcontext
+def teardown_appcontext(error=None):
+    # Automatically commit and close the session when the app context is torn down
+    if hasattr(g, 'session'):
+        g.session.commit()
+        g.session.close()
 
 def extract_laws_from_text(text):
-    
     doc = nlp(text)
     laws = []
 
     for ent in doc.ents:
         if ent.label_ == "LAW":
             laws.append(ent.text)
-    
+
     laws = list(set(laws))
     return laws
 
-
 def extract_text_from_html(html_content):
-    
     parsed_html = html.fromstring(html_content)
-    
-   
     div_texts = parsed_html.xpath('//div//text()')
-    
     text_content = " ".join(div_texts)
     cleaned_text = text_content.replace('\r\n', ' ')
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
-    cleaned_text = cleaned_text.rstrip()    
+    cleaned_text = cleaned_text.rstrip()
     return cleaned_text
-
 
 def load_data_from_database(table_name):
     try:
-        
-        engine = create_engine("mysql+pymysql://root:@localhost/taxin_revivedtaxindia_new1",pool_size=10, max_overflow=20)
-
-        query = f"SELECT * FROM {table_name}"
+        query = f"SELECT 'Id', 'Content' FROM {table_name}"
         df = pd.read_sql_query(query, engine)
-
         return df
-
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return None
 
-def dataframe_to_mysql(dataframe):
+def dataframe_to_mysql(dataframe, table_name):
     try:
-        
-        engine = create_engine("mysql+pymysql://root:@localhost/taxin_revivedtaxindia_new1", pool_size=10, max_overflow=20)
-        table_name = 'tbl_search_content_cleaned'
-
-        
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
         create_table_sql = text(f"CREATE TABLE IF NOT EXISTS `{table_name}` (" \
             "`Id` INT(15) NOT NULL, " \
             "`Clean_text` TEXT NULL DEFAULT NULL, " \
@@ -68,8 +67,8 @@ def dataframe_to_mysql(dataframe):
             "PRIMARY KEY (`Id`)" \
             ") ENGINE = InnoDB;")
 
-        session.execute(create_table_sql)
-        session.commit()
+        g.session.execute(create_table_sql)
+        g.session.commit()
         print(f"Table '{table_name}' has been successfully created in the taxin_revivedtaxindia_new1.")
 
         for index, row in dataframe.iterrows():
@@ -77,39 +76,69 @@ def dataframe_to_mysql(dataframe):
             Clean_text = row['Clean_text']
             laws = ', '.join(row['laws'])
 
-            
-            
-
             insert_data_sql = text(f"INSERT INTO `{table_name}` (`Id`, `Clean_text`, `laws`) " \
                 "VALUES (:Id, :Clean_text, :laws)")
-            session.execute(insert_data_sql, params={'Id': id, 'Clean_text': Clean_text, 'laws': laws})
-            
-        session.commit()
+            g.session.execute(insert_data_sql, params={'Id': id, 'Clean_text': Clean_text, 'laws': laws})
+
+        g.session.commit()
         print(f"DataFrame has been successfully converted to the '{table_name}' table in the database.")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-    finally:
-        if session:
-            session.close()
+@app.route("/<int:CaseId>/text", methods=["GET"])
+def get_text(CaseId):
+    query = f"SELECT Clean_text FROM tbl_search_content_cleaned WHERE Id = {CaseId}"
+    df = pd.read_sql_query(query, engine)
 
+    if df.empty:
+        return jsonify({"error": "No data found for the given CaseId"}), 404
+
+    clean_text = df["Clean_text"].iloc[0]
+
+    response_data = {"CaseId": CaseId, "CleanText": clean_text}
+
+    return jsonify(response_data), 200
+
+@app.route("/<int:CaseId>/laws", methods=["GET"])
+def get_laws(CaseId):
+    query = f"SELECT laws FROM tbl_search_content_cleaned WHERE Id = {CaseId}"
+    df = pd.read_sql_query(query, engine)
+
+    if df.empty:
+        return jsonify({"error": "No data found for the given CaseId"}), 404
+
+    laws = df["laws"].iloc[0]
+
+    response_data = {"CaseId": CaseId, "Laws": laws}
+
+    return jsonify(response_data), 200
+
+@app.route("/addcase", methods=["POST"])
+def add_case():
+    try:
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "Invalid JSON data provided"}), 400
+
+        CaseId = data["Id"]
+        judgement = data["Judgement"]
+        Clean_text = extract_text_from_html(judgement)
+        laws = extract_laws_from_text(Clean_text)
+        insert_data_sql = text(f"INSERT INTO tbl_search_content_cleaned (`Id`, `Clean_text`, `laws`) " \
+                "VALUES (:Id, :Clean_text, :laws)")
+        g.session.execute(insert_data_sql, params={'Id': CaseId, 'Clean_text': Clean_text, 'laws': ', '.join(laws)})
+        return jsonify({"message": "Case added successfully"}), 201
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == "__main__":
     original_table_name = 'tbl_search_content'  
-    df = load_data_from_database(original_table_name)
-    new_df = df[['Id', 'Content']]
+    new_df = load_data_from_database(original_table_name)
     new_df = new_df.rename(columns={'Content': 'Judgement'})
-    new_df['Clean_text'] = new_df['Judgement'].apply(lambda x: extract_text_from_html(x.decode('utf-8')))
+    new_df['Clean_text'] = new_df['Judgement'].apply(lambda x: extract_text_from_html(x.encode('utf-8')))
     new_df['laws'] = new_df['Clean_text'].apply(extract_laws_from_text)
-    fin_df= new_df[['Id', 'Clean_text', 'laws']]
+    fin_df = new_df[['Id', 'Clean_text', 'laws']]
     fin_df = fin_df.sort_values(by='Id')
-    dataframe_to_mysql(fin_df)
-
-    
-    
-    
-
-    
-    
-
+    dataframe_to_mysql(fin_df, 'tbl_search_content_cleaned')
+    app.run()
